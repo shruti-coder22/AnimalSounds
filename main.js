daad090531344dd3284941ced99e740117881346
shruti-coder22 <79634442+shruti-coder22@users.noreply.github.com>
2021-08-21 15:27:47 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,44 @@ function startClassification () {
 function modelReady() {
     classifier.classify(gotResults);
 }
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+
+        random_num_r = Math.floor(Math.random()*255)+1;
+        random_num_g = Math.floor(Math.random()*255)+1;
+        random_num_b = Math.floor(Math.random()*255)+1;
+
+        document.getElementById("result_label").style.color = "rgb(" + random_num_r + "," + random_num_g + "," + random_num_b + ")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_num_r + "," + random_num_g + "," + random_num_b + ")";
+
+        document.getElementById("result_label").innerHTML = "I can hear a " + results[0].label;
+        document.getElementById("result_confidence").innerHTML = "Acuracy = " + (results[0].confidence*100).toFixed(2) + " %";
+
+        img = document.getElementById("ear");
+
+
+
+
+       if (results[0].label == "Dog") {
+           img.src = "Dog.png";
+       }
+       else if (results[0].label == "Cat") {
+           img.src = "Cat.png";
+       }
+
+       else if (results[0].label == "Bird") {
+           img.src = "Bird.png";
+       }
+
+       else {
+           img.src = "Ear.png";
+       }
+
+
+   }
+}
